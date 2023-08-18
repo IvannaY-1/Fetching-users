f010c2709b7c78973e77af77670a3c82ddb3467e
Ivanna Yarynovska <ivanna.yarynovsk@gmail.com>
2023-08-17 23:11:33 -0400

    Step 2. Function renderUsers(users)

--- fetching-users/script.js
+++ fetching-users/script.js
@@ -22,3 +22,35 @@ async function loadUserData() {
   }
 }
 
+function renderUsers(users) {
+const container = document.getElementById("userDataDisplay");
+    const userCards = isLoading
+    ? "<p>Loading data...</p>"
+    : !users.length
+    ? "<p>No users found or no data available.</p>"
+    : users
+        .map((user) => {
+          const status = user.completed ? "✅ Completed" : "❌ Not Completed";
+          return `
+      <div class="user-card">
+        <h2>${user.name}</h2>
+        <p>Name: ${user.username}</p>
+        <p>Email: ${user.email}</p>
+        <p>Phone: ${user.phone}</p>
+        <p>Website: ${user.website}</p>
+        <h2>Address</h2>
+        <p>Street: ${user.street}</p>
+        <p>Suite: ${user.suite}</p>
+        <p>City: ${user.city}</p>
+        <p>Zip: ${user.zip}</p>
+        <h2>Company</h2>
+        <p>Name: ${user.company.name}</p>
+        <p>Catchphrase: ${user.Catchphrase}</p>
+        <p>BS: ${user.BS}</p>
+      </div>
+    `;
+        })
+        .join("");
+
+  container.innerHTML = userCards;
+}
